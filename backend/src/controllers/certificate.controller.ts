ef2d9385d7237920869f5e12ad19703d00e24afd
Sal-Zuba <alzubaidisadooon@gmail.com>
2025-03-04 08:59:47 +0100

    Fixing the Creation request in the backend

--- backend/src/controllers/certificate.controller.ts
+++ backend/src/controllers/certificate.controller.ts
@@ -4,8 +4,26 @@ import {Certificate} from "../interfaces/certificate";
 
 const createCertificate = async (req: Request, res: Response) => {
     try {
+        const {name, provider, filename, issued_at, expires_at } = req.body;
+        if (!name || !provider || !filename || !issued_at) {
+            return res.status(400).json({error: 'Missing required fields'})
+        }
+        
+        const issuedDate = new Date(issued_at);
+        const expirationDate = expires_at ? new Date(expires_at) : null;
+
+        if (expirationDate && expirationDate <= issuedDate) {
+            return res.status(400).json({ error: 'Expiration date must be after the issued date' });
+        }
+
         const newCertificate = await prisma.certificate.create({
-            data: {},
+            data: {
+                name,
+                provider,
+                filename,
+                issued_at: new Date(issued_at),
+                expires_at: expires_at ? new Date(expires_at) : null,
+            },
         })
 
         res.status(201).json(newCertificate)
@@ -17,7 +35,6 @@ const createCertificate = async (req: Request, res: Response) => {
 const getCertificates = async (req: Request, res: Response) => {
     try {
         const certificates: Certificate[] = await prisma.certificate.findMany()
-
         res.status(200).json(certificates)
     } catch (e) {
         console.error(e)
