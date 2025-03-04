48d11360434ec8413b9a7fd94328c55c953acee0
Sal-Zuba <alzubaidisadooon@gmail.com>
2025-03-04 09:05:09 +0100

    Adding a logging ssytem for errors in the backend that outputs every error in a server.log file using Winston, Also for now im allowing only POST and GET requests Will implement a DELETE endpoint afterwards too

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,38 @@
 import express, { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 import CertificateRouter from './routes/certificate.route'
+import winston from 'winston'
 
 export const prisma = new PrismaClient()
 
 const app = express()
 const port = 8080
-
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console({
+            level: 'info',  
+            format: winston.format.simple(),
+        }),
+        new winston.transports.File({
+            filename: 'server.log',
+            level: 'info',  
+        })
+    ]
+})
 async function main() {
     app.use(express.json())
 
     app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
         next()
     })
 
@@ -20,7 +41,9 @@ async function main() {
 
     // Catch unregistered routes
     app.all('*', (req: Request, res: Response) => {
-        res.status(404).json({ error: `Route ${req.originalUrl} not found` })
+        const errorMessage = `Route ${req.originalUrl} not found`
+        logger.error(errorMessage)
+        res.status(404).json({ error: errorMessage })
     })
 
     app.listen(port, () => {
@@ -29,21 +52,22 @@ async function main() {
 
     process.on('uncaughtException', (error) => {
         // Handle the uncaught exception here (e.g., log it)
-        console.error('Uncaught Exception:', error)
+        logger.error(`Uncaught Exception: ${error.message}`)
     })
 
     process.on('unhandledRejection', (reason, promise) => {
         // Handle unhandled promise rejections here (e.g., log them)
-        console.error('Unhandled Promise Rejection:', reason)
+        logger.error(`Unhandled Promise Rejection: ${reason}`)
     })
 }
+
 
 main()
     .then(async () => {
         await prisma.$connect()
     })
     .catch(async (e) => {
-        console.error(e)
+        logger.error(`Error during server startup: ${e.message}`)
         await prisma.$disconnect()
         process.exit(1)
     })
